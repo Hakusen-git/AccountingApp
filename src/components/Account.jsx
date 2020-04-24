bee128ab1c05b52ab8694f761e17bd0b1d74fcdd
wayne yao <wayneyao99@gmail.com>
2020-04-24 16:13:52 +1200

    Merged button animation into Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import './hoverButton.css';
 
 class Account extends Component {
-   
+    state = {
+        hover: false
+    }
+
+    toggleHover = (e) => {
+        this.setState({ hover: !this.state.hover });
+    }
+
     render() {
         
 
         const {id, type, catagory, detail, date, amount} = this.props.account;
+        
         
       
 
@@ -17,7 +24,11 @@ class Account extends Component {
                 <p style={pStyle}>{detail}</p>
                 <p style={pStyle}>{date}</p>
                 <p style={pStyle}>${amount}</p>
-                <button className="button"  onClick={this.props.delAccount.bind(this, id)} >Delete</button> 
+                <button style={this.state.hover ? {...buttonStyle, background: 'red'} : {...buttonStyle ,background: 'orangered'}} 
+                        className="button"  
+                        onMouseEnter={this.toggleHover} 
+                        onMouseLeave={this.toggleHover} 
+                        onClick={this.props.delAccount.bind(this, id)}>Delete</button> 
             </div>
         )
     }
@@ -31,6 +42,18 @@ const pStyle = {
     flex: '4',
     margin: '0'
 }
+
+const buttonStyle = {
+    flex: '1',
+    color: '#fff',
+    border: 'none',
+    padding: '5px 9px',
+    borderRadius: '25%',
+    cursor: 'pointer',
+    float: 'right',
+    transition: 'all 1s ease',
+}
+
 
 
 export default Account;
